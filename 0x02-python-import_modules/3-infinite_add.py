affa63d3ecaba9e951546e15032e3bb5d09a4be7
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-12 21:53:55 -0500

    [UPD]

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,9 @@
 if __name__ == "__main__":
     import sys
 
-    argc = len(sys.argv) -1
+    argc = len(sys.argv) - 1
     suma = 0
     for i in range(argc):
         suma = suma + int(sys.argv[i + 1])
+
     print(suma)
