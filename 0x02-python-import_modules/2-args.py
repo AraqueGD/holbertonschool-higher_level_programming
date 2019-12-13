5bc24d2638b5e1f860d18880eca78bba95ee2a4a
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-12 20:47:34 -0500

    [UPD]Fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
+    import sys
     argc = len(sys.argv) - 1
     if (argc == 0):
-        print("0 arguments.")
+        print("{} arguments.".format(argc))
     elif (argc == 1):
-        print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
+        print("{} argument:".format(argc))
+        print("{}: {}".format(argc, sys.argv[1]))
     else:
         print("{}: arguments:".format(argc))
         for i in range(len(sys.argv)):
