2a940583627d6e2815f6e802ac378c4381b93b17
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-18 10:18:00 -0500

    [Add]Max function

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return(None)
+    value = my_list[0]
+    for i in my_list:
+        if i > value:
+            value = i
+    return(value)
