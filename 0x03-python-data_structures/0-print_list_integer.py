b53bc23cf88d87f11f934a8b2cdacc942fdd1f6e
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-17 19:05:06 -0500

    FIxed

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 def print_list_integer(my_list=[]):
-  my_list = map(str, my_list)
-  for i in my_list:
-    print(str.format(i))
+    my_list = map(str, my_list)
+    for i in my_list:
+        print(str.format(i))
