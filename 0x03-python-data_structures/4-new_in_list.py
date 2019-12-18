df63c5e895940d787ba6f4edf5525ca88bc942b4
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-17 21:08:27 -0500

    Fixed

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if (idx < 0):
+        return (my_list)
+    elif (idx > len(new_list)):
+        return (my_list)
+    new_list.remove(new_list[idx])
+    new_list.insert(idx, element)
+    return (new_list)
