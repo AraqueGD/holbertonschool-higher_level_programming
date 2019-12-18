b9a138b437447d66044e91c31b2936632587ea8d
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-17 21:09:38 -0500

    Fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if (idx < 0):
         return (my_list)
-    elif (idx > len(new_list)):
+    elif (idx > len(new_list) - 1):
         return (my_list)
     new_list.remove(new_list[idx])
     new_list.insert(idx, element)
