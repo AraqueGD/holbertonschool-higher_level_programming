f676998ed186bbdc674b1d750621c10e2acc99ca
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-19 23:02:13 -0500

    [FIx]10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,11 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    bestKey = sorted(a_dictionary, reverse=True)
-    return bestKey[0]
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
