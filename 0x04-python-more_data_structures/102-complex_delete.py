d0c652f9a6714e62d18cb40d45d82e1d04db35f2
AraqueGD <camiaraquecaro17@gmail.com>
2019-12-20 16:37:55 -0500

    [ADD]15. Delete by value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new = a_dictionary.copy()
+    for key,valor in new.items():
+        if (valor == value):
+            del a_dictionary[key]
+    return(a_dictionary)
