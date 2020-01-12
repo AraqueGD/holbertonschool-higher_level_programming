895e78114b9435ff1858179b36502def2d878352
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-12 17:31:37 -0500

    [ADD]Point six

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0,0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if (type(value) != tuple or len(value) != 2):
+            raise TypeError ("position must be a tuple of 2 positive integers")
+        elif (value[0] <= 0 and type(value[1]) != int):
+            raise TypeError ("position must be a tuple of 2 positive integers")
+        elif (value[0] < 0 or value[1] < 0):
+            raise TypeError ("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                print("{}{}".format(' '*self.__position[0], '#'*self.__size))
