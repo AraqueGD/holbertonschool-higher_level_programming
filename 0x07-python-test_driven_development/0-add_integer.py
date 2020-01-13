9415259075b1bec2a6f24a41fd8c0016c905f05e
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-12 23:12:24 -0500

    [ADD]0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Program addition tow integer. """
+
+def add_integer(a, b=98):
+
+    """
+        Function Add Integers
+        and Errors.
+    """
+
+    if (isinstance(a, float) or isinstance(b, float)):
+        a = int(a)
+        b = int(b)
+    elif (type(a) is not int):
+        raise TypeError("a must be an integer")
+    elif (type(b) is not int):
+        raise TypeError("b must be an integer")
+    return a + b
