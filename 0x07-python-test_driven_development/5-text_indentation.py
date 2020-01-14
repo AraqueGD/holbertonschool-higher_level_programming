b88296f02f3cc457360aa97982d803fbf3e20130
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-13 23:07:58 -0500

    [ADD]4. Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Function Separate Text """
+
+def text_indentation(text):
+
+    """
+        The Function Separate Text If Find ".?:"
+        and Errors
+    """
+    sw = False
+    if (type(text) is not str):
+        raise TypeError("text must be a string")
+    for i in text:
+        if(i in ".?:"):
+            print(i, end="")
+            print("\n")
+            sw = True
+        else:
+            if(sw == True):
+                sw = False
+                continue
+            print(i, end="")
