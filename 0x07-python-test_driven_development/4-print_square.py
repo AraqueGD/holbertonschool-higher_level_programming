433b0f4650d17a56a35204a23c763c4f5bfd002a
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-13 21:54:21 -0500

    [ADD]3. Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Function Print Square """
+
+def print_square(size):
+    """
+        Function Print Square and
+        Erros Function
+    """
+    if (type(size) is not int):
+        raise TypeError("size must be an integer")
+    elif (size < 0):
+        raise ValueError("size must be >= 0")
+    elif (size < 0 and type(size) is float):
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print('#' * size, end="")
+        print()
