367c11150171019b46916745c14bb708a39b1e67
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-21 16:04:55 -0500

    [UPD]FIxed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 def read_file(filename=""):
     with open(filename, 'r', encoding="UTF-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
         f.close()
