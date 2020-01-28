962b160d8b89ee2e1d175efd8275f2a7bd1bb430
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-27 23:39:16 -0500

    [ADD ALL FILES]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """ Difine Base Class Module """
 
 import json
+import os
+import csv
 
 
 class Base:
@@ -18,8 +20,85 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ Json String function """
         if (list_dictionaries is None):
             list_dictionaries = []
             return list_dictionaries
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Save to fule json """
+        json_file = cls.__name__ + '.json'
+        new_list = []
+        if list_objs is not None:
+            for i in list_objs:
+                new_list.append(cls.to_dictionary(i))
+        with open(json_file, 'w') as fle:
+            fle.write(cls.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ From json to string """
+        if (json_string is None):
+            json_string = []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Create a instance """
+        if (cls.__name__ is "Rectangle"):
+            instance = cls(1, 1)
+        elif (cls.__name__ is "Square"):
+            instance = cls(1)
+        instance.update(**dictionary)
+        return instance
+
+    @classmethod
+    def load_from_file(cls):
+        """ Load from file json """
+        file_json = cls.__name__ + ".json"
+        new_list = []
+        if os.path.isfile(file_json):
+            with open(file_json, 'r') as f:
+                new_list = cls.from_json_string(f.read())
+            for i, j in enumerate(new_list):
+                new_list[i] = cls.create(**new_list[i])
+            return new_list
+        else:
+            return new_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ Save to file csv """
+        file_cvs = cls.__name__ + ".csv"
+        with open(file_cvs, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ Load file csv Function """
+        file_json = cls.__name__ + ".csv"
+        try:
+            with open(file_json, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -78,5 +78,78 @@ class test_to_json_string(unittest.TestCase):
             Base.to_json_string([], 2)
 
 
+class test_save_to_file(unittest.TestCase):
+    """ Testing Function Save to File json """
+
+    def test_save_to_file_one_rectangle(self):
+        """ Test Save to File Json """
+        r_test = Rectangle(10, 7, 2, 8, 5)
+        Rectangle.save_to_file([r_test])
+        with open("Rectangle.json", "r") as f:
+            self.assertTrue(len(f.read()) == 53)
+
+
+class test_from_json_string(unittest.TestCase):
+    """ Testing convert json to dictionary """
+
+    def test_from_json_string_type(self):
+        list_input = [{"id": 89, "width": 10, "height": 4}]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list, type(list_output))
+
+
+class test_crate(unittest.TestCase):
+    """ Testing Create Dictionary to Instance """
+
+    def test_create_rectangle_original(self):
+        r_test = Rectangle(3, 5, 1, 2, 7)
+        r_test_dictionary = r_test.to_dictionary()
+        r_test = Rectangle.create(**r_test_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r_test))
+
+
+class test_load_from_file(unittest.TestCase):
+    """ Testing Load From File """
+
+    def test_load_from_file_first_rectangle(self):
+        """ Test First rectangle """
+        r_test = Rectangle(10, 7, 2, 8, 1)
+        r_test2 = Rectangle(2, 4, 5, 6, 2)
+        Rectangle.save_to_file([r_test, r_test2])
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertEqual(str(r_test), str(list_rectangles_output[0]))
+
+    def test_load_from_file_square_types(self):
+        """ Test Second rectangle """
+        s_test = Square(5, 1, 3, 3)
+        s_test2 = Square(9, 5, 2, 3)
+        Square.save_to_file([s_test, s_test2])
+        output = Square.load_from_file()
+        self.assertTrue(all(type(obj) == Square for obj in output))
+
+
+class test_save_to_file_csv(unittest.TestCase):
+    """ Testing SAve to File csv """
+
+    def test_save_to_file_csv_one_rectangle(self):
+        """ Test save to file csv rectangle """
+        r_test = Rectangle(10, 7, 2, 8, 5)
+        Rectangle.save_to_file_csv([r_test])
+        with open("Rectangle.csv", "r") as f:
+            self.assertTrue("5,10,7,2,8", f.read())
+
+
+class test_load_from_file_csv(unittest.TestCase):
+    """ Testin load from file cvs """
+
+    def test_load_from_file_csv_first_rectangle(self):
+        r_test = Rectangle(10, 7, 2, 8, 1)
+        r_test = Rectangle(2, 4, 5, 6, 2)
+        Rectangle.save_to_file_csv([r_test, r_test])
+        list_rectangles_output = Rectangle.load_from_file_csv()
+        self.assertEqual(str(r_test), str(list_rectangles_output[0]))
+
+
 if __name__ == "__main__":
     unittest.main()
