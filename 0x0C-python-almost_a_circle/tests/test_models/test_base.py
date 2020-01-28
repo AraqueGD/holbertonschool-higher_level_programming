d604a7931729264c16f73cbc37b9084851e824c5
AraqueGD <camiaraquecaro17@gmail.com>
2020-01-28 11:38:42 -0500

    Full Checker

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,7 @@
 """ Unit Testing for Models/base.py """
 
 import unittest
+import pep8
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
@@ -149,6 +150,14 @@ class test_load_from_file_csv(unittest.TestCase):
         Rectangle.save_to_file_csv([r_test, r_test])
         list_rectangles_output = Rectangle.load_from_file_csv()
         self.assertEqual(str(r_test), str(list_rectangles_output[0]))
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(
+            ['models/rectangle.py', 'models/base.py', 'models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 
 if __name__ == "__main__":
