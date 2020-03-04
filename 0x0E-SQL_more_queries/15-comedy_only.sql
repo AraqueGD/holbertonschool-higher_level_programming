07c4be3c0e89ca9696f1206324b2eff8e7c46f6b
AraqueGD <camiaraquecaro17@gmail.com>
2020-03-03 23:40:49 -0500

    [ADD]15. Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -6,4 +6,4 @@ ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE name = "Comedy"
-ORDER BY tv_genres.title ASC;
+ORDER BY tv_shows.title ASC;
