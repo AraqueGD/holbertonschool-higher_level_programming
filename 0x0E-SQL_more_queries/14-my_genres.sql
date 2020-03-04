baf13d2d0fff65fdaa99e4d5a0949ed094953088
AraqueGD <camiaraquecaro17@gmail.com>
2020-03-03 23:21:43 -0500

    [ADD]14. My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
-ON tv_shows_genres.genre.id = tv_genres.id
+ON tv_shows_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 WHERE title = Dexter
