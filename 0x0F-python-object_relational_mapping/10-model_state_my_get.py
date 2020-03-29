49846c4d7dfe73a3d8fab4ede983eace4f782c91
AraqueGD <camiaraquecaro17@gmail.com>
2020-03-29 18:43:31 -0500

    [ADD]10. Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,10 +5,10 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
+    aux = 0
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    aux = 0
     Base.metadata.create_all(engine)
     session = Session(engine)
 
