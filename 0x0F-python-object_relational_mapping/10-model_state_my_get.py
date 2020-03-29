21883a4a5bc53f8124ad4f7a8f2e3a8d4cff7b27
AraqueGD <camiaraquecaro17@gmail.com>
2020-03-29 18:40:31 -0500

    [ADD]10. Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,13 +8,12 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    name_db = argv[4]
     aux = 0
     Base.metadata.create_all(engine)
     session = Session(engine)
 
     for state in session.query(State).order_by(State.id).\
-            filter(State.name == name_db):
+            filter(State.name == argv[4]):
         print("{}".format(state.id))
         aux = 1
     if (aux == 0):
