2733d697dfe2dfa4759c4227617598a03d0dff93
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-14 21:51:42 -0500

    [ADD]4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import requests
+
+    url = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t - type: {}".format(type(url.text)))
+    print("\t - content: {}".format(url.text))
