1fd4079cf75371a1046a686659b571906a4cee14
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-15 16:59:11 -0500

    Fix Checker

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
-        print("\t - type: {}".format(type(response.read())))
-        print("\t - content: {}".format(html))
-        print("\t - utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- type: {}".format(type(response.read())))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
