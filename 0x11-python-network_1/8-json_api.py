15c43385b9bead449b7534cb71d3b6ff8cd4b90f
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-15 18:51:58 -0500

    [ADD]8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,12 +3,13 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
+    url = 'http://0.0.0.0:5000/search_user'
     if (len(argv) > 1):
         dic = {'q': argv[1]}
     else:
         dic = {'q': ""}
     try:
-        url = requests.post('http://0.0.0.0:5000/search_user', data=dic)
+        url = requests.post(url, dic)
         js = url.json()
 
         if (not js):
