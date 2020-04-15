6fcc2b644c4a9bdb14b9a7d06916b09857e597da
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-15 17:18:55 -0500

    Fix Tab

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
 
     url = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t - type: {}".format(type(url.text)))
-    print("\t - content: {}".format(url.text))
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.text))
