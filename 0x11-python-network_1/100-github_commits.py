44ed89ad7851b25eff6ba7e413c2c7bf6cecd63d
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-15 20:58:07 -0500

    [ADD]10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,8 +3,10 @@ if __name__ == "__main__":
     from requests import get, auth
     from sys import argv
 
+    repo = argv[1]
+    user = argv[2]
     url = get(
-        'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2]))
+        'https://api.github.com/repos/{}/{}/commits'.format(repo, user))
     try:
         js = url.json()
         for ten in js[:10]:
