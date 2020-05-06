9d7477cf0a19a448f0632b3ce25651b377a05e57
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 20:38:07 -0500

    [Fix]Checkers

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // Write a script that prints 3 lines
-let one = "C is Fun";
-let two = "Python is cool";
-let three = "Javascript is amazing";
-console.log(one);
-console.log(two);
-console.log(three);
+const listLenguages = ['C is Fun', 'Python is cool', 'Javascript is amazing'];
+for (const eachList in listLenguages) {
+  console.log(listLenguages[eachList]);
+}
