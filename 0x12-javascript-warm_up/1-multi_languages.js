f67ba12322befcd6d752cf4d074f072f55618af1
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 20:28:51 -0500

    [ADD]1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines
+let list_lenguages = ["C is Fun", "Python is cool", "Javascript is amazing"];
+for (let each_list in list_lenguages) {
+  console.log(list_lenguages[each_list]);
+}
