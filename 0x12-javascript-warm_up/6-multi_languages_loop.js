10b3f55789ddc468a451921934e0625aca654695
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 23:19:20 -0500

    [ADD]6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Print Multi lines in Array with Loop.
+const listLenguages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const lenguages in listLenguages) {
+  console.log(listLenguages[lenguages]);
+}
