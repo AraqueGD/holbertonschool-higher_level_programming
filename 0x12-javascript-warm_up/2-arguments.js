91996de04fb14a431a08ec53aa9bf8888f873715
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 21:26:30 -0500

    [ADD]2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Write a script that prints a message depending of the number of arguments passed.
-let args = process.argv
+const args = process.argv;
 
 if ((args.length === 2)) {
-    console.log('No argument')
+  console.log('No argument');
 } else if ((args.length === 3)) {
-    console.log('Argument Found')
+  console.log('Argument Found');
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found');
 }
