d5084cbf93f7e2389f79b488d476c1fa52c7f8f6
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 22:55:45 -0500

    [UPD]ADD function isNaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 
 const number = parseInt(args[0], 10);
 
-if ((isNaN(number))) {
+if ((Number.isNaN(number))) {
   console.log('Not a Number');
 } else {
   console.log('My number: ' + number);
