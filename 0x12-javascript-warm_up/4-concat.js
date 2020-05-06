8fd73f04599c66164c41f2a9f2e441a87eeff1c8
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 22:23:41 -0500

    [ADD]4. Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Write a script that prints two arguments passed to it, in the following format: “ is ”
+const args = process.argv.slice(2);
+
+const one = args[0]
+const two = args[1]
+
+if ((!one)) {
+  console.log(one + ' is ' + two)
+} else if ((one.length === 1)) {
+  console.log(one + ' is ' + two)
+} else {
+  console.log(one + ' is ' + two)
+}
