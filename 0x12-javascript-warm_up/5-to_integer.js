bee0badadae6b28b0427a9a357e34d8c555961a3
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-05 23:01:07 -0500

    [Fix]

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,6 +6,6 @@ const number = parseInt(args[0]);
 
 if ((Number.isNaN(number))) {
   console.log('Not a Number');
-} else {
+} else if ((typeof number === 'number')){
   console.log('My number: ' + number);
 }
