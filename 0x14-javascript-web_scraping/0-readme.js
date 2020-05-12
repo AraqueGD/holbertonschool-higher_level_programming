6c8c4b831e7f9c637c229f0e55454896ca696672
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-12 18:11:02 -0500

    [ADD]0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,9 +7,10 @@
 */
 const fs = require('fs');
 const args = process.argv.slice(2);
-try {
-  const file = fs.readFileSync(args[0], 'utf-8');
-  console.log(file);
-} catch (err) {
-  console.error(err);
-}
+fs.readFile(args[0], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
