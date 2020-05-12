2ee0bfd869e031274773fbf9a817e65431505385
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-11 22:46:11 -0500

    [ADD]11. Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Write a script that imports an array and computes a new array.
-const list = require('./100-data');
+const list = require('./100-data').list;
 const map1 = list.map(function(num, index){
-    return (num * index);
+    return (index * num);
 });
 console.log(list);
 console.log(map1);
