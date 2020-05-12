430f21b0443ee31f3e099da83cd6083b337f08d7
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-11 21:20:46 -0500

    [ADD]6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@
     - Create an instance method called charPrint(c) that prints the rectangle using the character c
         -If c is undefined, use the character X
 */
-const Square5 = require('5-square.');
+const Square5 = require('./5-square');
 class Square extends Square5 {
   charPrint (c) {
     if (typeof (c) === 'undefined') {
