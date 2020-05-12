497dc02fdf13cfad6ee5950f5601d06a73ec789c
AraqueGD <camiaraquecaro17@gmail.com>
2020-05-11 23:07:22 -0500

    [ADD]12. Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+const dic = require('./101-data').dict;
+const newDict = {};
+Object.keys(dic).map(function (key, index) {
+    if (newDict[dic[key]] === undefined) {
+      newDict[dic[key]] = [];
+    }
+    newDict[dic[key]].push(key);
+  });
+  console.log(newDict);
